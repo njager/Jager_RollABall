b455afe9ad979cf5ab096fe837547aa73ceae911
njager <nicholasejager@gmail.com>
2020-09-27 11:46:39 -0400

    added tags for pickups

--- Jager_RollABall/Assets/Scripts/PlayerController.cs
+++ Jager_RollABall/Assets/Scripts/PlayerController.cs
@@ -32,4 +32,9 @@ public class PlayerController : MonoBehaviour
 
         rb.AddForce(movement * speed);
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        other.gameObject.SetActive(false);
+    }
 }

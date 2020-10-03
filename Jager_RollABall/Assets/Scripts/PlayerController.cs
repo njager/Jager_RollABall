850cb7915e7f8e111f90a0c04be5177dbd53a069
njager <nicholasejager@gmail.com>
2020-10-03 18:50:50 -0400

    placed final pick ups

--- Jager_RollABall/Assets/Scripts/PlayerController.cs
+++ Jager_RollABall/Assets/Scripts/PlayerController.cs
@@ -53,7 +53,7 @@ public class PlayerController : MonoBehaviour
     void SetCountText()
     {
         countText.text = "Count: " + count.ToString();
-        if(count >= 12)
+        if(count >= 27)
         {
             winTextObject.SetActive(true);
         }
